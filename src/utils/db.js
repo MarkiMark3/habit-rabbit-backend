86e0c2e362694bc0d8af6848cb06723c7ea2ab26
Markiyan Dmyterko <marki.dmyterko@gmail.com>
2026-03-06 16:20:46 +0100

    redeploy

--- src/utils/db.js
+++ src/utils/db.js
@@ -10,7 +10,7 @@ import { Sequelize } from "sequelize";
 // });
 
 export const client = new Sequelize(
-  "postgresql://postgres:yFrxqtDhgGvCkjFGXKnhYpXegRxVraSY@shortline.proxy.rlwy.net:36324/railway",
+  "postgresql://postgres:pZIpxbIrHLZEyZoNpmujcuBEczqHBHlV@ballast.proxy.rlwy.net:43323/railway",
   {
     dialect: "postgres",
     protocol: "postgres",
